9b117382d5d414461d11fb2ae04690e498cd5c52
Malena de Arriba <dearribamalena@gmail.com>
2023-03-27 22:35:03 -0300

    ejercicio 1 corregido

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -5,10 +5,12 @@ let auto = {
     año: 2022,
     puertas: 5,
     encendido: function () {
+        encenderAuto = alert("Quieres encender el auto?");
         document.write("<p>El auto está encendido</p>");
     },
     apagado: function () {
-        document.write("<p>Apagado</p>");
+        apagarAuto = alert("Quieres apagar el auto?");
+        document.write("<p>El auto se apagó</p>");
     }
 } 
 
